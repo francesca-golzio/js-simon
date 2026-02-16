909644cf37e46a447b372a84f41bc80858ea8310
Francesca Golzio <francescagolzio@gmail.com>
2026-02-16 16:19:24 +0100

    split task in subtasks

--- js/script.js
+++ js/script.js
@@ -1,10 +1,42 @@
 /* is it working? */
 console.log('🟢 yes, it is');
 
+/* Genero 5 numeri casuali */
+/* - salvo i 5 valori in un array ('randoNumb')
+     (dopo mi servono per il confronto) */
+
+/* Stampo in pagina i numeri casuali */
+/* nella lista 'ul.numbers-list' */
+
+/* (nello stesso momento)
+   Parte un timer di 30 secondi */
+
+/* quando finiscono i 30 secondi */
+/* - i numeri scompaiono */
+/* - le istruzioni (p#instructions) cambiano
+     'Inserisci i numeri che hai visto (nell'ordine che preferisci).'
+*/
+
+
+/* - appaiono i 5 input 'form.answers-form' */
+
+/* - al CLICK del bottone 'button' */
+/* -- controllo che non ci siano duplicati */ // ✨ BONUS
+/* -- e che siano tutti numeri */ // ✨ BONUS
+/* -- salvo i numeri inseriti dall'Utente in un array ('userNumb') */
+
+/* - controllo la corrispondenza tra numeri mostrati e inseriti 
+ovvero, controllo se i due array hanno elementi comuni
+- NB. l'utente può inserire i numeri in un ordine qualsiasi
+*/
+/* - salvo i numeri comuni in un nuovo array 'memorizedNumb' */
+
+/* stampo in pagina (in p.message) */
+/* - quanti numeri (memorizedNumb.lenght) sono stati memorizzati correttamente */
+/* - e quali sono questi numeri (array memorizedNumb) */
 
 
 
-
-
-
-
+/* - le istruzioni (p#instructions) cambiano
+     'Memorizza i numeri entro il tempo limite!'
+*/
