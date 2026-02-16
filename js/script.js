3db7de691dfa7167ce66bb07531878d4b9cb0325
Francesca Golzio <francescagolzio@gmail.com>
2026-02-16 18:20:49 +0100

    save user numbers

--- js/script.js
+++ js/script.js
@@ -6,8 +6,11 @@ const countdownEl = document.getElementById('countdown');
 const instructionsEl = document.getElementById('instructions');
 const numbersListEl = document.getElementById('numbers-list');
 const formEl = document.getElementById('answers-form');
-/* DUBBIO: 👇 forse vanno presi singolarmente (es. ennesimo child di 🤔) */
-const userInputsEl = document.querySelector('div.input-group > input.form-control');
+const userInputEl1 = document.getElementById('numb-1');
+const userInputEl2 = document.getElementById('numb-2');
+const userInputEl3 = document.getElementById('numb-3');
+const userInputEl4 = document.getElementById('numb-4');
+const userInputEl5 = document.getElementById('numb-5');
 const buttonEl = document.querySelector('button');
 const messageEl = document.getElementById('message');
 
@@ -20,26 +23,25 @@ const memorizedNumbs = [/* Qui recupera elementi comuni */];
 /* Genero 5 numeri casuali */
 for(i = 1; i <= 5; i++){
   number = getRandomNumbBetween(1, 50);
-  console.log(number);
+  //console.log(number);
 
   /* - salvo i 5 valori in un array ('randoNumbs')
        (dopo mi servono per il confronto) */
   randoNumbs.push(number);
-  console.log(randoNumbs);  
+  //console.log(randoNumbs);  
 }
 /* Stampo in pagina i numeri casuali */
 /* nella lista 'ul.numbers-list' */
 for(x = 0; x < randoNumbs.length; x++){
   numbersListEl.innerHTML += `<li>${randoNumbs[x]}</li>`;
 }
-/* (nello stesso momento)
-   Parte un timer di 30 secondi */
+/* Imposta un timer di 30 secondi */
 setTimeout(function() {
   /* quando finiscono i 30 secondi */
   /* - i numeri scompaiono */
-  console.log(numbersListEl.classList);
+  //console.log(numbersListEl.classList);
   numbersListEl.classList.add('d-none');
-  console.log(numbersListEl.classList);
+  //console.log(numbersListEl.classList);
   
   /* - le istruzioni (p#instructions) cambiano
        'Inserisci i numeri che hai visto (nell'ordine che preferisci).' */
@@ -48,11 +50,24 @@ setTimeout(function() {
        formEl.classList = "";
 }, 2000/* 30000 */);
 
-
 /* - al CLICK del bottone 'button' */
+buttonEl.addEventListener('click', function(e) {
+e.preventDefault();
+
 /* -- controllo che non ci siano duplicati */ // ✨ BONUS
 /* -- e che siano tutti numeri */ // ✨ BONUS
 /* -- salvo i numeri inseriti dall'Utente in un array ('userNumbs') */
+  let number = userInputEl1.value;
+  userNumbs.push(number);
+  number = userInputEl2.value;
+  userNumbs.push(number);
+  number = userInputEl3.value;
+  userNumbs.push(number);
+  number = userInputEl4.value;
+  userNumbs.push(number);
+  number = userInputEl5.value;
+  userNumbs.push(number);
+  //console.log(userNumbs);
 
 /* - controllo la corrispondenza tra numeri mostrati e inseriti 
 ovvero, controllo se i due array hanno elementi comuni
@@ -63,6 +78,8 @@ ovvero, controllo se i due array hanno elementi comuni
 /* stampo in pagina (in p.message) */
 /* - quanti numeri (memorizedNumbs.lenght) sono stati memorizzati correttamente */
 /* - e quali sono questi numeri (array memorizedNumbs) */
+
+})
 
 
 
